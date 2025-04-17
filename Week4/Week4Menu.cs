74bec32b08fed45d1ed5dc10acd917b402d8fc72
Ricardo Tanaka Terukina <ricardotanaka-externo@tgacompany.com>
2025-04-16 20:00:03 -0500

    Created week 4 menu

---
+++ Week4/Week4Menu.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Progra1251.Week4
+{
+    internal class Week4Menu : Menu
+    {
+        public override void Execute()
+        {
+            Console.WriteLine("Menu de la semana 4");
+        }
+    }
+}
